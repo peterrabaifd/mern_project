41054321939d5969ac8a6d61bc5a8b7fcd1878fe
2394958r <2394958r@student.gla.ac.uk>
2020-10-29 15:51:07 +0000

    episode 2 finished

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const multer = require('multer')
 
 const UserController = require('./controllers/UserController')
 const EventController = require('./controllers/EventController')
+const DashboardController = require('./controllers/DashboardController')
 const uploadConfig = require('./config/upload')
+const LoginController = require('./controllers/LoginController')
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,9 +14,23 @@ routes.get('/status', (req, res) => {
     res.send({ status: 200 })
 })
 
+//TODO Login Controller
+//TODO Subscribe Controller
+//TODO Approval Controller
+//TODO Rejection Controller
+
+//Login
+routes.post('/login', LoginController.store)
+
+
+//Dashboard
+routes.get('/dashboard/:sport', DashboardController.getAllEvents)
+routes.get('/dashboard', DashboardController.getAllEvents)
+routes.get('/event/:eventId', DashboardController.getEventById)
+
 
 //Event
-routes.get('/event/:eventId', EventController.getEventById)
+routes.delete('/event/:eventId', EventController.delete)
 routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
 
 
