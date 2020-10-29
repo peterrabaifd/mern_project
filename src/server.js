8f8387070f619f607fcf078f986a6cb267c3a8ea
2394958r <2394958r@student.gla.ac.uk>
2020-10-29 14:38:22 +0000

    episode 1 finished

--- src/server.js
+++ src/server.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const routes =require('./routes');
+const path = require("path");
 const app = express();
-const RegisterController = require('./controllers/UserController')
-
-const PORT = process.env.PORT || 8000;
-
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
-
+const PORT = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello from express')
-})
-
-app.post('/register', RegisterController.store)
-
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
 
 try {
-    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    console.log('MongoDB connected')
-} catch (error){
-    console.log(error)
+	mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	console.log('MongoDb connected successfully!')
+} catch (error) {
+	console.log(error)
 }
+
+app.use("/files", express.static(path.resolve(__dirname, "..", "files")))
+app.use(routes);
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)
